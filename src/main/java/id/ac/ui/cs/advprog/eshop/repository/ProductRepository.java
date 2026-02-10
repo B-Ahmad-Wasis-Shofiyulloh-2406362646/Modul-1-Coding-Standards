6cb5a6ee8837e1081ca1118a3ea3bc2d836b6acd
Ahmad Wasis <ahmadwasissh@gmail.com>
2026-02-10 12:47:09 +0700

    update product repository for edit

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,41 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            product.setProductId(UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findById(String id) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product update(Product product) {
+        for (Product productInRepo : productData) {
+            if (productInRepo.getProductId().equals(product.getProductId())) {
+                productInRepo.setProductName(product.getProductName());
+                productInRepo.setProductQuantity(product.getProductQuantity());
+                return productInRepo;
+            }
+        }
+        return null; 
     }
 }
